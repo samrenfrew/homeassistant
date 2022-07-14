283b4b530b5fbabbb2d87037125b757f392f7010
Sam <contact@samrenfrew.co.uk>
2022-07-14 08:13:10 +0100

    2022.6.7

--- git_backup.sh
+++ git_backup.sh
@@ -3,7 +3,8 @@
 sudo git add .
 sudo git status
 echo -n "Enter version number: " [Minor Update]
-read CHANGE_MSG
+CHANGE_MSG=$(cat .HA_VERSION)
+#read CHANGE_MSG
 sudo git commit -m "${CHANGE_MSG}"
 git push origin master
 
